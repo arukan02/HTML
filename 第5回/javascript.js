96664ad77f920543c97d0bf529a6f2a95828ade5
arukan02 <87008720+arukan02@users.noreply.github.com>
2022-12-11 21:10:39 +0900

    Delete 第5回 directory

--- 第5回/javascript.js
+++
@@ -1,5 +0,0 @@
-function compare(){
-	var a=document.getElementById("other").value;
-	var answer = (a>270203917) ? "人口が多い国はあなたのお国です!":"人口が多い国は私の国です!";
-	document.getElementById("answer").innerHTML = answer;
-}
